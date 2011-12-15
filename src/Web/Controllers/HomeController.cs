84997180e2590552bddab5ee43af442365f07ab1
Tim G. Thomas <tim@timgthomas.com>
2011-12-14 20:17:43 -0600

    Basic HTTP request functionality

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet, ActionName("SendRequest")]
+		public JsonResult SendRequest_Get(string name)
+		{
+			return Json(new { message = GetMessage(name, "GET") }, JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpPost, ActionName("SendRequest")]
+		public JsonResult SendRequest_Post(string name)
+		{
+			return Json(new { message = GetMessage(name, "POST") });
+		}
+
+		[HttpPut, ActionName("SendRequest")]
+		public JsonResult SendRequest_Put(string name)
+		{
+			return Json(new { message = GetMessage(name, "PUT") });
+		}
+
+		[HttpDelete, ActionName("SendRequest")]
+		public JsonResult SendRequest_Delete(string name)
+		{
+			return Json(new { message = GetMessage(name, "DELETE") });
+		}
+
+		private string GetMessage(string name, string verb)
+		{
+			return string.Format("{0}, you just sent a {1} request.", name, verb);
+		}
+	}
+}
